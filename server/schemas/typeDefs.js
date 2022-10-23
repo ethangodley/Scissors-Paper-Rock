51b7c5a87c59f719e93e814704537d2179a0e024
Ethan Godley <62864699+ethangodley@users.noreply.github.com>
2022-10-23 16:37:46 +0800

    schemas setup including resolvers and typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,33 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Profile {
+    _id: ID
+    name: String
+    email: String
+    password: String
+    scores: [Int]!
+  }
+
+  type Auth {
+    token: ID!
+    profile: Profile
+  }
+
+  type Query {
+    profiles: [Profile]!
+    profile(_id: ID!): Profile
+    # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
+    me: Profile
+  }
+
+  type Mutation {
+    addProfile(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    addScore(_id: ID!, score: Int!): Profile
+    removeProfile: Profile
+  }
+`;
+
+module.exports = typeDefs;
